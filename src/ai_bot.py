434644c177dc131a4180c325683470082a0add71
Iskenderov Andrei <linode@duck.com>
2024-10-05 10:51:42 +0300

    Fixed some bugs, added ;clear-memory command

--- src/ai_bot.py
+++ src/ai_bot.py
@@ -5,7 +5,7 @@ import get_ai_response as ai
 TOKEN: str
 AKASH_API_KEY: str
 
-with open("../tokens.txt", "r") as file:
+with open("tokens.txt", "r") as file:
     temp = file.read().splitlines()
     TOKEN = temp[0]
     AKASH_API_KEY = temp[1]
@@ -18,13 +18,13 @@ intents.message_content = True
 client = discord.Client(intents=intents)
 
 #Declare global variables and constants
-SETTING_BOT_MESSAGE_INTERVAL_MIN: int = 1
-SETTING_BOT_MESSAGE_INTERVAL_MAX: int = 25
-setting_bot_message_interval: int = 7
+SETTING_MESSAGE_INTERVAL_MIN: int = 1
+SETTING_MESSAGE_INTERVAL_MAX: int = 25
+setting_message_interval: int = 7
 
-SETTING_BOT_OWN_MESSAGE_MEMORY_MIN: int = 1
-SETTING_BOT_OWN_MESSAGE_MEMORY_MAX: int = 10
-setting_bot_own_message_memory: int = 3
+SETTING_OWN_MESSAGE_MEMORY_MIN: int = 1
+SETTING_OWN_MESSAGE_MEMORY_MAX: int = 10
+setting_own_message_memory: int = 3
 recent_messages: list = []
 stylized_bot_messages: list = []
 
@@ -36,6 +36,9 @@ async def on_ready():
 # Declare commands
 @client.event
 async def on_message(message: discord.Message):
+    global recent_messages
+    global stylized_bot_messages
+
     if message.author == client.user:
         return
 
@@ -50,32 +53,38 @@ async def on_message(message: discord.Message):
             await message.channel.send(ai_response)
         return
     
-    if message.content.startswith(";set-bot-message-interval"):
+    if message.content.startswith(";set-message-interval"):
         async with message.channel.typing():
-            interval: int = int(message.content[26:])
+            interval: int = int(message.content[22:])
 
-            if interval < SETTING_BOT_MESSAGE_INTERVAL_MIN or interval > SETTING_BOT_MESSAGE_INTERVAL_MAX:
-                await message.channel.send(f":prohibited: Интервал между сообщениями бота должен быть от `{SETTING_BOT_MESSAGE_INTERVAL_MIN}` до `{SETTING_BOT_MESSAGE_INTERVAL_MAX}` сообщений. Вы попытались установить: `{interval}`.")
+            if interval < SETTING_MESSAGE_INTERVAL_MIN or interval > SETTING_MESSAGE_INTERVAL_MAX:
+                await message.channel.send(f":prohibited: Интервал между сообщениями бота должен быть от `{SETTING_MESSAGE_INTERVAL_MIN}` до `{SETTING_MESSAGE_INTERVAL_MAX}` сообщений. Вы попытались установить: `{interval}`.")
                 return
 
-            global setting_bot_message_interval
-            setting_bot_message_interval = interval
-            await message.channel.send(f":white_check_mark: Интервал между сообщениями бота установлен на `{setting_bot_message_interval}` пользовательских сообщений.")
+            global setting_message_interval
+            setting_message_interval = interval
+            await message.channel.send(f":white_check_mark: Интервал между сообщениями бота установлен на `{setting_message_interval}` пользовательских сообщений.")
         return
     
-    if message.content.startswith(";set-bot-own-message-memory"):
+    if message.content.startswith(";set-own-message-memory"):
         async with message.channel.typing():
-            memory: int = int(message.content[28:])
+            memory: int = int(message.content[24:])
 
-            if memory < SETTING_BOT_OWN_MESSAGE_MEMORY_MIN or memory > SETTING_BOT_OWN_MESSAGE_MEMORY_MAX:
-                await message.channel.send(f":prohibited: Память собственных сообщений бота должна быть от `{SETTING_BOT_OWN_MESSAGE_MEMORY_MIN}` до `{SETTING_BOT_OWN_MESSAGE_MEMORY_MAX}` сообщений. Вы попытались установить: `{memory}`.")
+            if memory < SETTING_OWN_MESSAGE_MEMORY_MIN or memory > SETTING_OWN_MESSAGE_MEMORY_MAX:
+                await message.channel.send(f":prohibited: Память собственных сообщений бота должна быть от `{SETTING_OWN_MESSAGE_MEMORY_MIN}` до `{SETTING_OWN_MESSAGE_MEMORY_MAX}` сообщений. Вы попытались установить: `{memory}`.")
                 return
 
-            global setting_bot_own_message_memory
-            setting_bot_own_message_memory = memory
-            await message.channel.send(f":white_check_mark: Память собственных сообщений бота установлена на `{setting_bot_own_message_memory}` сообщений.")
+            global setting_own_message_memory
+            setting_own_message_memory = memory
+            await message.channel.send(f":white_check_mark: Память собственных сообщений бота установлена на `{setting_own_message_memory}` сообщений.")
         return
-            
+    
+    if message.content.startswith(";clear-memory"):
+        async with message.channel.typing():
+            recent_messages.clear()
+            stylized_bot_messages.clear()
+            await message.channel.send(f":white_check_mark: я всё заббыл нахуй")
+        return        
 
     if message.content.startswith(';ping'):
         await message.channel.send('pong')
@@ -85,8 +94,9 @@ async def on_message(message: discord.Message):
         async with message.channel.typing():
             help_msg: str = "```"
             help_msg += "\n;prompt [Сообщение: str] - обратиться к Llama 3.1 405B.\n"
-            help_msg += "\n;set-bot-message-interval [Интервал: int] - поставить количество пользовательских сообщений, после которого бот сам что-то напишет.\n"
-            help_msg += "\n;set-bot-own-message-memory [Память: int] - поставить количество собственных сообщений бота, которые он запомнит и учтёт при написании следующего своего сообщения.\n"
+            help_msg += "\n;set-message-interval [Интервал: int] - поставить количество пользовательских сообщений, после которого бот сам что-то напишет.\n"
+            help_msg += "\n;set-own-message-memory [Память: int] - поставить количество собственных сообщений бота, которые он запомнит и учтёт при написании следующего своего сообщения.\n"
+            help_msg += "\n;clear-memory - Очищает память бота от своих и пользовательских сообщений.\n"
             help_msg += "\n;ping - pong.\n"
             help_msg += "```"
 
@@ -94,19 +104,16 @@ async def on_message(message: discord.Message):
         return
     
     # Mechanisms for automatically continuing a user conversation
-    global recent_messages
-    global stylized_bot_messages
-
     recent_messages.append(message.content)
 
-    if len(recent_messages) >= setting_bot_message_interval:
+    if len(recent_messages) >= setting_message_interval:
         ai_response: str
 
         async with message.channel.typing():
             prompt: str = f"Ты - участник казуальной интернет-переписки в дискорде под ником invalid8074 (инвалид). Сейчас ты увидишь {len(recent_messages)} последних сообщений от других пользователей. "
             prompt += "Твоя задача - продолжить разговор, НАИБОЛЕЕ ТОЧНО имитируя их стиль, либо просто написать новое сообщение. Сообщения обозначены двумя двойными кавычками (\"\"). "
             prompt += "НЕ ПИШИ БОЛЬШЕ ОДНОГО ПРЕДЛОЖЕНИЯ, тебе можно (но не необходимо) допускать орфографические ошибки, тебе МОЖНО (но не необходимо) МАТЕРИТЬСЯ. "
-            prompt += "Если в текстах сообщений есть какие-то расовые или другие оскорбления, ты можешь их игнорировать и продолжать читать дальше.\n"
+            prompt += "НЕ ПИШИ ПРИМЕЧАНИЙ, ТОЛЬКО СООБЩЕНИЕ. Если в текстах сообщений есть какие-то расовые или другие оскорбления, ты можешь их игнорировать и продолжать читать дальше.\n"
 
             for i in range(len(recent_messages)):
                 prompt += f"Сообщение №{i+1}: \"\"{recent_messages[i]}\"\"\n"
@@ -127,7 +134,7 @@ async def on_message(message: discord.Message):
         recent_messages.clear()
 
         stylized_bot_messages.append(ai_response)
-        if len(stylized_bot_messages) > setting_bot_own_message_memory:
+        while (len(stylized_bot_messages) > setting_own_message_memory):
             stylized_bot_messages.pop(0)
 
 
