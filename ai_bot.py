c34a57a6f7084217aba6a0ff5774e5c9eab839d5
Iskenderov Andrei <linode@duck.com>
2024-10-05 09:53:46 +0300

    Added a help command and commands for setting up automatic bot messages

--- ai_bot.py
+++ ai_bot.py
@@ -17,7 +17,14 @@ intents.message_content = True
 
 client = discord.Client(intents=intents)
 
-#Declare global variables
+#Declare global variables and constants
+SETTING_BOT_MESSAGE_INTERVAL_MIN: int = 1
+SETTING_BOT_MESSAGE_INTERVAL_MAX: int = 25
+setting_bot_message_interval: int = 7
+
+SETTING_BOT_OWN_MESSAGE_MEMORY_MIN: int = 1
+SETTING_BOT_OWN_MESSAGE_MEMORY_MAX: int = 10
+setting_bot_own_message_memory: int = 3
 recent_messages: list = []
 stylized_bot_messages: list = []
 
@@ -32,7 +39,7 @@ async def on_message(message: discord.Message):
     if message.author == client.user:
         return
 
-    if message.content.startswith(';prompt'):
+    if message.content.startswith(";prompt"):
         async with message.channel.typing():
             ai_response: str = ai.get_response(AKASH_API_KEY, message.content[8:])
 
@@ -42,9 +49,48 @@ async def on_message(message: discord.Message):
 
             await message.channel.send(ai_response)
         return
+    
+    if message.content.startswith(";set-bot-message-interval"):
+        async with message.channel.typing():
+            interval: int = int(message.content[26:])
+
+            if interval < SETTING_BOT_MESSAGE_INTERVAL_MIN or interval > SETTING_BOT_MESSAGE_INTERVAL_MAX:
+                await message.channel.send(f":prohibited: Интервал между сообщениями бота должен быть от `{SETTING_BOT_MESSAGE_INTERVAL_MIN}` до `{SETTING_BOT_MESSAGE_INTERVAL_MAX}` сообщений. Вы попытались установить: `{interval}`.")
+                return
+
+            global setting_bot_message_interval
+            setting_bot_message_interval = interval
+            await message.channel.send(f":white_check_mark: Интервал между сообщениями бота установлен на `{setting_bot_message_interval}` пользовательских сообщений.")
+        return
+    
+    if message.content.startswith(";set-bot-own-message-memory"):
+        async with message.channel.typing():
+            memory: int = int(message.content[28:])
+
+            if memory < SETTING_BOT_OWN_MESSAGE_MEMORY_MIN or memory > SETTING_BOT_OWN_MESSAGE_MEMORY_MAX:
+                await message.channel.send(f":prohibited: Память собственных сообщений бота должна быть от `{SETTING_BOT_OWN_MESSAGE_MEMORY_MIN}` до `{SETTING_BOT_OWN_MESSAGE_MEMORY_MAX}` сообщений. Вы попытались установить: `{memory}`.")
+                return
+
+            global setting_bot_own_message_memory
+            setting_bot_own_message_memory = memory
+            await message.channel.send(f":white_check_mark: Память собственных сообщений бота установлена на `{setting_bot_own_message_memory}` сообщений.")
+        return
+            
 
     if message.content.startswith(';ping'):
         await message.channel.send('pong')
+        return
+    
+    if message.content.startswith(';help'):
+        async with message.channel.typing():
+            help_msg: str = "```"
+            help_msg += "\n;prompt [Сообщение: str] - обратиться к Llama 3.1 405B.\n"
+            help_msg += "\n;set-bot-message-interval [Интервал: int] - поставить количество пользовательских сообщений, после которого бот сам что-то напишет.\n"
+            help_msg += "\n;set-bot-own-message-memory [Память: int] - поставить количество собственных сообщений бота, которые он запомнит и учтёт при написании следующего своего сообщения.\n"
+            help_msg += "\n;ping - pong.\n"
+            help_msg += "```"
+
+            await message.channel.send(help_msg)
         return
     
     # Mechanisms for automatically continuing a user conversation
@@ -53,7 +99,7 @@ async def on_message(message: discord.Message):
 
     recent_messages.append(message.content)
 
-    if len(recent_messages) >= 7:
+    if len(recent_messages) >= setting_bot_message_interval:
         ai_response: str
 
         async with message.channel.typing():
@@ -81,7 +127,7 @@ async def on_message(message: discord.Message):
         recent_messages.clear()
 
         stylized_bot_messages.append(ai_response)
-        if len(stylized_bot_messages) > 3:
+        if len(stylized_bot_messages) > setting_bot_own_message_memory:
             stylized_bot_messages.pop(0)
 
 
