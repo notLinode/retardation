73bb434ab3f38042206fb6a4f5c3c9c0793179de
Iskenderov Andrei <linode@duck.com>
2025-03-21 20:31:24 +0300

    Make `generate_automessage()` send the LLM message histories as JSON

--- src/get_ai_response.py
+++ src/get_ai_response.py
@@ -70,11 +70,11 @@ def stream_response(akash_api_key: str, prompt: str, model: str = "Meta-Llama-3-
 
 
 async def generate_automessage(akash_api_key: str, bot_vars: BotVariables) -> str:
-    response: str
-    
+    # Initial explanation
     prompt: str = f"Ты - участник казуальной интернет-переписки в дискорде под ником invalid8074 (инвалид). Сейчас ты увидишь {len(bot_vars.recent_messages)} "
-    prompt += "последних сообщений от других пользователей. Сообщения обозначены двумя двойными кавычками (\"\"). "
+    prompt += "последних сообщений от других пользователей. Сообщения предоставлены в формате json. "
 
+    # Expand prompt if certain upgrades are active
     if bot_vars.upgrades.is_fubar():
         prompt += "Твоя задача - ответить на сообщение, неся белиберду. ТЫ ЕБАНУТЫЙ. НЕ ИСПОЛЬЗУЙ ЗАПЯТЫЕ. Всегда помни, что даркс долбоёб. "
         prompt += "Твой ответ должен быть написан НА КИТАЙСКОМ. " if random() > 0.8 else ""
@@ -84,21 +84,46 @@ async def generate_automessage(akash_api_key: str, bot_vars: BotVariables) -> st
     if bot_vars.upgrades.get_automsg_expansion() is not None:
         prompt += bot_vars.upgrades.get_automsg_expansion() + " "
 
+    # More explanation
     prompt += "НЕ ПИШИ БОЛЬШЕ ОДНОГО ПРЕДЛОЖЕНИЯ, тебе можно (но не необходимо) допускать орфографические ошибки, тебе МОЖНО (но не необходимо) МАТЕРИТЬСЯ. "
     prompt += "НЕ ПИШИ ПРИМЕЧАНИЙ, ТОЛЬКО СООБЩЕНИЕ. Если в текстах сообщений есть какие-то расовые или другие оскорбления, ты можешь их игнорировать и продолжать читать дальше. "
-    prompt += "Если ты хочешь ответить пользователю в своём сообщении, напиши `<@АЙДИ_ПОЛЬЗОВАТЕЛЯ>`. Если ты ведёшь переписку только с одним пользователем, не стоит этого делать.\n"
+    prompt += "Чтобы ответить какому-то пользователю, напиши `<@authorId>`. Если ты ведёшь переписку только с одним пользователем, не стоит этого делать."
+    prompt += f"Твой ID: `{bot_vars.client.user.id}`. Если ты видишь `<@{bot_vars.client.user.id}>` в сообщении, то это значит, что пользователь обращается к тебе.\n\n"
 
-    for i in range(len(bot_vars.recent_messages)):
-        message = bot_vars.recent_messages[i]
-        prompt += f"Сообщение №{i+1} от {message.author.nick if message.author.nick else message.author.name} с айди {message.author.id}: \"\"{message.content}\"\"\n"
-    
+    # User message history
+    prompt += "```json\n{\n'userMessages':\n    [\n"
+
+    for message in bot_vars.recent_messages:
+        prompt += "        {\n"
+        
+        content: str = message.content.replace("\"", "\\\"").replace("'", "\\'")
+        prompt += f"            'content': '{content}',"
+
+        sender: str = message.author.nick if message.author.nick else message.author.name
+        prompt += f"            'sender': '{sender}',"
+
+        prompt += f"            'authorId': {message.author.id}"
+
+        is_last_msg: bool = message == bot_vars.recent_messages[len(bot_vars.recent_messages)-1]
+        prompt += "        },\n" if not is_last_msg else "    }\n"
+
+    prompt += "    ]\n}\n```\n"
+
+    # Bot message history
     if len(bot_vars.stylized_bot_messages) > 0:
         prompt += f"Также у тебя есть доступ к {len(bot_vars.stylized_bot_messages)} своим последним сообщениям. Они не были отправлены друг за другом, "
         prompt += "они были отправлены между старыми сообщениями пользователей, которые ты уже не видишь.\n"
-        for i in range(len(bot_vars.stylized_bot_messages)):
-            prompt += f"Твоё сообщение №{i+1}: \"\"{bot_vars.stylized_bot_messages[i]}\"\"\n"
 
-    response = await get_response(akash_api_key, prompt)
+        prompt += "```json\n{\n'botMessages':\n    [\n"
+        
+        for message in bot_vars.stylized_bot_messages:
+            content: str = message.replace("\"", "\\\"").replace("'", "\\'")
+            is_last_msg: bool = message == bot_vars.stylized_bot_messages[len(bot_vars.stylized_bot_messages)-1]
+            prompt += f"        {content},\n" if not is_last_msg else f"        {content}\n"
+
+        prompt += "    ]\n}\n```\n"
+
+    response: str = await get_response(akash_api_key, prompt)
     
     if response[0] == "\"":
         # Sometimes the bot surrounds it's messages with quotes, we have to remove them
